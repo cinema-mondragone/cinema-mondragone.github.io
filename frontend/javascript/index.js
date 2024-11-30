14458339d64255d502cfd8fe87c6f523020128b3
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2024-11-30 12:07:05 +0100

    First commit

---
+++ frontend/javascript/index.js
@@ -0,0 +1,8 @@
+import "$styles/index.css"
+
+// Import all JavaScript & CSS files from src/_components
+import components from "$components/**/*.{js,jsx,js.rb,css}"
+
+import 'flowbite';
+
+console.info("Bridgetown is loaded!")
